a47d25ff141e237fa0701ea72ffd8af0cd4e6752
Megapreneur <ademolamegbabi@gmail.com>
2022-07-10 14:47:30 +0100

    my first Go commit

--- fizzBuzz.go
+++ fizzBuzz.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func main() {
+	fizzBuzz()
+}
+func fizzBuzz() {
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+
+}
