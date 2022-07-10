d5979b4b697bab8629a35cea56ad5c599ecf238f
Megapreneur <ademolamegbabi@gmail.com>
2022-07-10 14:56:13 +0100

    my first Go commit

--- factorial.go
+++ factorial.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+	factorial(5)
+
+}
+func factorial(number int) {
+	result := 1
+	for i := 1; i <= number; i++ {
+		result *= i
+
+	}
+	fmt.Println("The factorial of", number, "is ", result)
+}
